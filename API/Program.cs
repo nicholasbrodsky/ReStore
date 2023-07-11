901067e2cccef92ee384e1ca63d761e0d4b0f4cf
Nicholas Brodsky <nicholas.brodsky@abbott.com>
2023-07-11 16:23:31 -0700

    Adds initial data elements

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 // create web app host to run app
 // runs app via kestrel server
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StoreContext>(options => {
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
