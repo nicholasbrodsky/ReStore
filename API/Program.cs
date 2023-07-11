68afbd2fe2e0928f41e846e3e3c4174d695e5f00
Nicholas Brodsky <nicholas.brodsky@abbott.com>
2023-07-11 14:27:32 -0700

    Updates initial settings for project

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
+// create web app host to run app
+// runs app via kestrel server
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// dep inj container - when we want to use a service inside a class in our proj (inj service to be used inside the app)
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,14 +12,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// Configure the HTTP request pipeline. - Middleware
+// api handles request coming in (handles what happens between request coming in and response being sent out)
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
